a311e4c183edf88ae215db4a52d449e519451e04
Stephan Wessels <stephan@g2design.co.za>
2016-09-14 15:37:14 +0200

    Added a session wrapper to framework related classes

--- src/G2Design/G2App/Base.php
+++ src/G2Design/G2App/Base.php
@@ -2,7 +2,14 @@
 
 namespace G2Design\G2App;
 
+use Aura\Session;
+
 class Base {
+	/**
+	 *
+	 * @var Session\Session
+	 */
+	static $session = null;
 
 	function get_module_dir($module = false) {
 
@@ -21,6 +28,19 @@ class Base {
 		list($junk, $package) = $paths_arr;
 		return \G2Design\G2App::get_module_dir($file_uri).'/';
 		
+	}
+	
+	/**
+	 * 
+	 * @return Session\Session;
+	 */
+	function session() {
+		if(!isset(self::$session)) {
+			$factory = new Session\SessionFactory();
+			self::$session = $factory->newInstance($_COOKIE);
+		}
+		
+		return self::$session;
 	}
 
 }
