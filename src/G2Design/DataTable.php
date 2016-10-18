fcb2145219a4b4130bbd55bbe26e4ec038403512
stefanAtG2 <stefan@g2design.co.za>
2016-10-18 11:40:17 +0200

    [Maint] Removed add field duplicate function

--- src/G2Design/DataTable.php
+++ src/G2Design/DataTable.php
@@ -29,10 +29,6 @@ class DataTable extends Table {
 	function set_fields($fields) {
 		$this->fields = $fields;
 	}
-	
-	function add_field($field){
-		array_push($this->fields, $field);
-	}
 
 	function add_field($field) {
 		array_push($this->fields, $field);
