7955e1947c85907722f3cf6378885bb830d97456
Stephan Wessels <stephan@g2design.co.za>
2016-11-04 10:05:13 +0200

    Added enviroment loader to Config class

--- src/G2Design/Config.php
+++ src/G2Design/Config.php
@@ -3,22 +3,42 @@
 namespace G2Design;
 
 class Config extends ClassStructs\Singleton {
+
 	var $config = null;
-	
+
 	static function load($file) {
 		$in = self::getInstance();
-		
+
 		$in->config = \Zend\Config\Factory::fromFile($file, true);
 	}
-	
+
+	static function load_env($env) {
+		$in = self::getInstance();
+
+		if (!$in->config) {
+			throw new Exception("First Load the configuration file with ::load()");
+		}
+
+		//Find the enviroments config area
+		if (isset($in->config->enviroments->{$env})) {
+			$enviroment = $in->config->enviroments->$env;
+			if(is_string($enviroment) && file_exists($enviroment)) {
+				$enviroment = \Zend\Config\Factory::fromFile($enviroment,true);
+			} else if (is_string($enviroment)) {
+				throw new Exception("String was provide in configuration file. But is not a valid file");
+			}
+			$in->config->merge($enviroment);
+		}
+		
+	}
+
 	/**
 	 * 
 	 * @return \Zend\Config
 	 */
-	static function get(){
+	static function get() {
 		$in = self::getInstance();
 		return $in->config;
 	}
-	
-	
+
 }
