50e4efca769f52635538d84a1b9566bc3e73dc71
Stephan Wessels <stephan@g2design.co.za>
2017-01-19 12:04:14 +0200

    Add loadconfig extensions
    [dev] Configs can now depend on other configs
    [dev] Configs can now load other configs into exiting config

--- src/G2Design/Config.php
+++ src/G2Design/Config.php
@@ -9,7 +9,52 @@ class Config extends ClassStructs\Singleton {
 	static function load($file) {
 		$in = self::getInstance();
 
-		$in->config = \Zend\Config\Factory::fromFile($file, true);
+//		$in->config = \Zend\Config\Factory::fromFile($file, true);
+		$reader = new \Zend\Config\Reader\Json();
+		$config = $reader->fromFile($file);
+		$in->config = new \Zend\Config\Config($config, true);
+		//Create depends script
+
+		if (isset($in->config->depends)) { // depends exists
+			foreach ($in->config->depends as $conf) {
+				if (is_string($conf) && file_exists($conf)) {
+//					$dp_conf = \Zend\Config\Factory::fromFile($conf, true);
+					$config = $reader->fromFile($conf);
+					$dp_conf = new \Zend\Config\Config($config, true);
+					$conf_new = $dp_conf->merge($in->config);
+					$in->config = $conf_new;
+				}
+			}
+		}
+
+		//Complete all load functions
+		self::load_config($in->config);
+	}
+
+	static function load_config(&$config) {
+		try {
+			$new_config = [];
+			foreach ($config as $key => $value) {
+				if (is_string($value) && strpos($value, 'load-config:') !== false) { // Found Load Path
+					$file = str_replace('load-config:', '', $value);
+					
+					$reader = new \Zend\Config\Reader\Json();
+					$c_ar = $reader->fromFile($file);
+					$new_config = array_merge($new_config, $c_ar);
+							
+							
+				} else {
+					$config->{$key} = self::load_config($value);
+				}
+			}
+			
+			$config = empty($new_config) ? $config : new \Zend\Config\Config($new_config, true);
+
+			return $config;
+		} catch (Exception $ex) {
+			echo $ex->getMessage();
+			exit;
+		}
 	}
 
 	static function load_env($env) {
@@ -22,14 +67,13 @@ class Config extends ClassStructs\Singleton {
 		//Find the enviroments config area
 		if (isset($in->config->enviroments->{$env})) {
 			$enviroment = $in->config->enviroments->$env;
-			if(is_string($enviroment) && file_exists($enviroment)) {
-				$enviroment = \Zend\Config\Factory::fromFile($enviroment,true);
+			if (is_string($enviroment) && file_exists($enviroment)) {
+				$enviroment = \Zend\Config\Factory::fromFile($enviroment, true);
 			} else if (is_string($enviroment)) {
 				throw new Exception("String was provide in configuration file. But is not a valid file");
 			}
 			$in->config = $in->config->merge($enviroment);
 		}
-		
 	}
 
 	/**
