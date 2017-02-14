4d0d637c26c15c096db704b095704a6f0caf2ed8
Stephan Wessels <stephan@g2design.co.za>
2017-02-14 14:14:10 +0200

    [dev] Added slugify function

--- src/G2Design/Utils/Functions.php
+++ src/G2Design/Utils/Functions.php
@@ -231,5 +231,37 @@ class Functions {
 
 		return $filetype;
 	}
+	
+	/**
+	 * Creates an urlsave version of a string
+	 * 
+	 * @param type $text
+	 * @return string
+	 */
+	static public function slugify($text) {
+		// replace non letter or digits by -
+		$text = preg_replace('~[^\pL\d]+~u', '-', $text);
+
+		// transliterate
+		$text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+
+		// remove unwanted characters
+		$text = preg_replace('~[^-\w]+~', '', $text);
+
+		// trim
+		$text = trim($text, '-');
+
+		// remove duplicate -
+		$text = preg_replace('~-+~', '-', $text);
+
+		// lowercase
+		$text = strtolower($text);
+
+		if (empty($text)) {
+			return 'n-a';
+		}
+
+		return $text;
+	}
 
 }
